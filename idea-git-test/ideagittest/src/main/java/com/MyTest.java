cb167deef9cc6d2f590a56464cf0fa698b519de9
zhao1 <zhan1@qq.com>
2019-12-22 18:56:16 +0800

    加了一句话

--- idea-git-test/ideagittest/src/main/java/com/MyTest.java
+++ idea-git-test/ideagittest/src/main/java/com/MyTest.java
@@ -5,6 +5,8 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("Hello,git!");
 
-        System.out.println("hello,github");
+        System.out.println("hello,github!");
+
+        System.out.println("hello,world!");
     }
 }
